17d1d27e2f4f4a0fac8aabbdfcb9692fe722311b
Danila Zhogol <danzhog@gmail.com>
2019-12-09 13:09:56 +0300

    changes made

---
+++ module4-5/src/Laba_4/Main.java
@@ -0,0 +1,15 @@
+package Laba_4;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Scanner;
+import  java.io.IOException;
+public class Main {
+    public static void main(String[] args)
+    {
+        MainFrame frame=new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
