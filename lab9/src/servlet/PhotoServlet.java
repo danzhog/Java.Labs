35c95c4b5e0317c6d60674fb98b2fe9318cb2f75
Danila Zhogol <danzhog@gmail.com>
2020-06-05 23:30:47 +0300

    Initial commit

---
+++ lab9/src/servlet/PhotoServlet.java
@@ -0,0 +1,43 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+public class PhotoServlet extends HttpServlet {
+    private JFileChooser fileChooser;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = new PhotoFrame().go();
+        System.out.println(path);
+
+        getServletContext().setAttribute("photo", path);
+
+        System.out.println(1);
+
+        resp.sendRedirect("/testing/updateAd.jsp");
+    }
+
+    public class PhotoFrame extends JFrame {
+        public PhotoFrame() {
+        }
+
+        public String go() {
+
+            if (fileChooser == null) {
+                fileChooser = new JFileChooser();
+                fileChooser.setCurrentDirectory(new File("."));
+            }
+            if (fileChooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
+                return fileChooser.getSelectedFile().toString();
+            }
+
+            return "";
+        }
+    }
+}
